5ddde03979f48ea591f28ea8c164d861e7cfb726
Terrionna <tkmcghee@gmail.com>
2017-01-24 20:00:29 -0500

    added html content to index.html to establish base layout for the home page of twitterish

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ server.listen (port,function (){
   console.log('the server is now listening on port',port);
 });
 server.get('/', function(request,response){
-  response.sendfile('index.html')
-}
+  response.sendfile('index.html', {root: __dirname+ '/public/html'});
+});
+server.get('/login' function(request,respose){
+response.sendfile('index.html', {root: __dirname+ '/public/html'});
+});
+server.get('/signup' function(request,respose){
+  respose.sendfile('index.html', ){root: __dirname+ 'public/html'});
+});
