920ccb89a12ba44f4b424c49eca00365179cf8f0
Terrionna <tkmcghee@gmail.com>
2017-01-24 20:42:06 -0500

    made entire public directory accessible as static folder of server

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var express = require ('express');
 var server = express ();
 var port =8080;
+
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', function(request,response){
+  response.sendFile('index.html', {root: __dirname+ '/public/html'});
+});
+server.get('/login', function(request, response){
+ response.sendFile('login.html', { root: __dirname+ '/public/html/'});
+});
+server.get('/signup', function(request,respose){
+  respose.sendFile('signup.html', {root: __dirname+ 'public/html'});
+});
+
 server.listen (port,function (){
   console.log('the server is now listening on port',port);
-});
-server.get('/', function(request,response){
-  response.sendfile('index.html', {root: __dirname+ '/public/html'});
-});
-server.get('/login' function(request,respose){
-response.sendfile('index.html', {root: __dirname+ '/public/html'});
-});
-server.get('/signup' function(request,respose){
-  respose.sendfile('index.html', ){root: __dirname+ 'public/html'});
 });
